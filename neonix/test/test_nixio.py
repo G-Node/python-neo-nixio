2921ab1ad3f58f9451257fda76ab8795c92ea8c9
Achilleas Koutsou <achilleas.k@gmail.com>
2016-01-22 19:13:18 +0200

    Basic testfile structure

--- neonix/test/test_nixio.py
+++ neonix/test/test_nixio.py
@@ -6,3 +6,23 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
+
+import os
+import unittest
+
+from neo.core import Block
+
+from neonix.io import nixio
+
+
+class NixIOTest(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = "nixio_testfile.hd5"
+        self.neo_block = Block(name="test", description="test description")
+        self.io = nixio()
+
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
