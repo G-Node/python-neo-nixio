8e1c3cb8513f1dcc715cab9599fc1afae22d56b7
Achilleas Koutsou <achilleas.k@gmail.com>
2016-01-25 20:06:54 +0200

    Changes to tests for file opening change

--- neonix/test/test_nixio.py
+++ neonix/test/test_nixio.py
@@ -13,27 +13,24 @@ import unittest
 from neo.core import Block, Segment
 
 from neonix.io.nixio import NixIO
-import nix
 
 
 class NixIOTest(unittest.TestCase):
-    # TODO: Simplify tests - lots of code duplication could go in setUp/tearDown
 
     def setUp(self):
         self.filename = "nixio_testfile.hd5"
         self.io = NixIO(self.filename)
 
     def tearDown(self):
+        del self.io
         if os.path.exists(self.filename):
             os.remove(self.filename)
 
     def test_block(self):
         neo_block = Block(name="test_block", description="block for testing")
         self.io.write_block(neo_block)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_block = nix_file.blocks[0]
+        nix_block = self.io.nix_file.blocks[0]
         self.assertTrue(NixIO._equals(neo_block, nix_block))
-        nix_file.close()
 
     def test_block_cascade(self):
         neo_block = Block(name="test_block", description="block for testing")
@@ -41,8 +38,7 @@ class NixIOTest(unittest.TestCase):
                               description="segment for testing")
         neo_block.segments.append(neo_segment)
         self.io.write_block(neo_block, cascade=True)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_block = nix_file.blocks[0]
+        nix_block = self.io.nix_file.blocks[0]
         self.assertEqual(nix_block.name, neo_block.name)
         self.assertEqual(nix_block.type, "neo.block")
         self.assertEqual(nix_block.definition, neo_block.description)
@@ -51,7 +47,6 @@ class NixIOTest(unittest.TestCase):
         self.assertEqual(nix_group.type, "neo.segment")
         self.assertEqual(nix_group.definition, neo_segment.description)
         self.assertTrue(NixIO._equals(neo_block, nix_block))
-        nix_file.close()
 
     def test_segment(self):
         neo_block = Block(name="test_block", description="block for testing")
@@ -59,13 +54,11 @@ class NixIOTest(unittest.TestCase):
         neo_segment = Segment(name="test_segment",
                               description="segment for testing")
         self.io.write_segment(neo_segment, neo_block)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_group = nix_file.blocks[0].groups[0]
+        nix_group = self.io.nix_file.blocks[0].groups[0]
         self.assertEqual(nix_group.name, neo_segment.name)
         self.assertEqual(nix_group.type, "neo.segment")
         self.assertEqual(nix_group.definition, neo_segment.description)
         self.assertTrue(NixIO._equals(neo_segment, nix_group))
-        nix_file.close()
 
     def test_missing_block(self):
         neo_block = Block(name="test_block", description="block for testing")
@@ -78,11 +71,9 @@ class NixIOTest(unittest.TestCase):
         neo_block = Block(name="test_block_neq",
                           description="block for testing neq")
         self.io.write_block(neo_block)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_block = nix_file.blocks[0]
+        nix_block = self.io.nix_file.blocks[0]
         neo_block.name = "foo"  # changing neo block name
         self.assertFalse(NixIO._equals(neo_block, nix_block))
-        nix_file.close()
 
     def test_segment_neq(self):
         neo_block = Block(name="test_block", description="block for testing")
@@ -90,11 +81,9 @@ class NixIOTest(unittest.TestCase):
                               description="segment for testing neq")
         neo_block.segments.append(neo_segment)
         self.io.write_block(neo_block, cascade=True)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_block = nix_file.blocks[0]
+        nix_block = self.io.nix_file.blocks[0]
         neo_segment.name = "foo"  # changing neo segment (child) name
         self.assertFalse(NixIO._equals(neo_block, nix_block))
-        nix_file.close()
 
     def test_container_len_neq(self):
         neo_block = Block(name="test_block", description="block for testing")
@@ -102,13 +91,15 @@ class NixIOTest(unittest.TestCase):
                               description="segment for testing")
         neo_block.segments.append(neo_segment)
         self.io.write_block(neo_block, cascade=True)
-        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nix_block = nix_file.blocks[0]
+        nix_block = self.io.nix_file.blocks[0]
         neo_segment_new = Segment(name="test_segment_2",
                                   description="second segment for testing")
         neo_block.segments.append(neo_segment_new)
         self.assertFalse(NixIO._equals(neo_block, nix_block))
-        nix_file.close()
+
+    def test_metadata(self):
+        self.fail("Implement metadata test")
+        pass
 
 if __name__ == "__main__":
     unittest.main()
