7d95264b8580461453fd5359857e0233eb0f9f6d
Achilleas Koutsou <achilleas.k@gmail.com>
2016-01-25 15:30:09 +0200

    Underscore delimited prefix variable names in test file

--- neonix/test/test_nixio.py
+++ neonix/test/test_nixio.py
@@ -27,26 +27,26 @@ class NixIOTest(unittest.TestCase):
             os.remove(self.filename)
 
     def test_block(self):
-        neoblock = Block(name="test_block", description="block for testing")
-        self.io.write_block(neoblock)
-        nixfile = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nixblock = nixfile.blocks[0]
-        self.assertEqual(nixblock.name, neoblock.name)
-        self.assertEqual(nixblock.type, "neo.block")
-        self.assertEqual(nixblock.definition, neoblock.description)
-        nixfile.close()
+        neo_block = Block(name="test_block", description="block for testing")
+        self.io.write_block(neo_block)
+        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
+        nix_block = nix_file.blocks[0]
+        self.assertEqual(nix_block.name, neo_block.name)
+        self.assertEqual(nix_block.type, "neo.block")
+        self.assertEqual(nix_block.definition, neo_block.description)
+        nix_file.close()
 
     def test_segment(self):
-        neoblock = Block(name="test_block", description="block for testing")
-        self.io.write_block(neoblock)
-        neosegment = Segment(name="test_segment",
-                             description="segment for testing")
-        self.io.write_segment(neosegment, neoblock)
-        nixfile = nix.File.open(self.filename, nix.FileMode.ReadOnly)
-        nixgroup = nixfile.blocks[0].groups[0]
-        self.assertEqual(nixgroup.name, neosegment.name)
-        self.assertEqual(nixgroup.type, "neo.segment")
-        self.assertEqual(nixgroup.definition, neosegment.description)
+        neo_block = Block(name="test_block", description="block for testing")
+        self.io.write_block(neo_block)
+        neo_segment = Segment(name="test_segment",
+                              description="segment for testing")
+        self.io.write_segment(neo_segment, neo_block)
+        nix_file = nix.File.open(self.filename, nix.FileMode.ReadOnly)
+        nix_group = nix_file.blocks[0].groups[0]
+        self.assertEqual(nix_group.name, neo_segment.name)
+        self.assertEqual(nix_group.type, "neo.segment")
+        self.assertEqual(nix_group.definition, neo_segment.description)
 
 if __name__ == "__main__":
     unittest.main()
