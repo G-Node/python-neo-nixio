944bf4acae3c8a44c0cb16f9bb841acaec5a6a3a
Achilleas Koutsou <achilleas.k@gmail.com>
2016-02-08 16:23:24 +0200

    Add coordinate values and units to metadata section

--- neonix/io/nixio.py
+++ neonix/io/nixio.py
@@ -192,9 +192,12 @@ class NixIO(BaseIO):
         if hasattr(rcg, "coordinates"):
             source_metadata = self._get_or_init_metadata(nix_source,
                                                          object_path)
-            nix_coordinates = NixIO._copy_coordinates(rcg.coordinates)
+            nix_coord_values, nix_coord_units = NixIO._copy_coordinates(
+                rcg.coordinates)
             source_metadata.create_property("coordinates",
-                                            nix_coordinates)
+                                            nix_coord_values)
+            source_metadata.create_property("coordinates.units",
+                                            nix_coord_units)
         # TODO: For each child object, create a source
         # https://github.com/G-Node/python-neo-nixio/blob/master/mapping.md#neorecordingchannelgroup
 
