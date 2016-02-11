e01d616ae3194e4f1ca5a2caa70263076733a954
Achilleas Koutsou <achilleas.k@gmail.com>
2016-02-11 23:25:33 +0200

    Test writing annotations to metadata section

--- neonix/test/test_nixio.py
+++ neonix/test/test_nixio.py
@@ -13,6 +13,7 @@ import unittest
 
 import numpy as np
 import quantities as pq
+import string
 
 import nix
 from neo.core import (Block, Segment, RecordingChannelGroup, AnalogSignal,
@@ -149,7 +150,78 @@ class NixIOTest(unittest.TestCase):
         self.io.write_all_blocks(blocks)
 
     def test_annotations(self):
-        self.fail("Write annotations test")
+        def rand_word():
+            return "".join(np.random.choice(list(string.ascii_letters), 10))
+
+        def rand_dict(nitems):
+            rd = dict()
+            for _ in range(nitems):
+                key = rand_word()
+                value = rand_word() if np.random.choice((0, 1))\
+                    else np.random.uniform()
+                rd[key] = value
+            return rd
+
+        times = np.array([1])*pq.s
+        signal = np.array([1])*pq.V
+        blk = Block()
+        blk.annotate(**rand_dict(3))
+
+        seg = Segment()
+        seg.annotate(**rand_dict(4))
+        blk.segments.append(seg)
+
+        asig = AnalogSignal(signal=signal, sampling_rate=pq.Hz)
+        asig.annotate(**rand_dict(2))
+        seg.analogsignals.append(asig)
+
+        isig = IrregularlySampledSignal(times=times, signal=signal,
+                                        time_units=pq.s)
+        isig.annotate(**rand_dict(2))
+        seg.irregularlysampledsignals.append(isig)
+
+        epoch = Epoch(times=times, durations=times)
+        epoch.annotate(**rand_dict(4))
+        seg.epochs.append(epoch)
+
+        event = Event(times=times)
+        event.annotate(**rand_dict(4))
+        seg.events.append(event)
+
+        spiketrain = SpikeTrain(times=times, t_stop=pq.s, units=pq.s)
+        spiketrain.annotate(**rand_dict(6))
+        seg.spiketrains.append(spiketrain)
+
+        rcg = RecordingChannelGroup(channel_indexes=[1, 2])
+        rcg.annotate(**rand_dict(4))
+        blk.recordingchannelgroups.append(rcg)
+
+        unit = Unit()
+        unit.annotate(**rand_dict(2))
+        rcg.units.append(unit)
+
+        nixblk = self.io.write_block(blk)
+
+        self.check_equal_attr(blk, nixblk)
+        self.check_equal_attr(seg, nixblk.groups[0])
+        for signal in [da for da in nixblk.data_arrays
+                       if da.type == "neo.analogsignal"]:
+            self.check_equal_attr(asig, signal)
+        for signal in [da for da in nixblk.data_arrays
+                       if da.type == "neo.irregularlysampledsignal"]:
+            self.check_equal_attr(isig, signal)
+        nixepochs = [mtag for mtag in nixblk.groups[0].multi_tags
+                     if mtag.type == "neo.epoch"]
+        self.check_equal_attr(epoch, nixepochs[0])
+        nixevents = [mtag for mtag in nixblk.groups[0].multi_tags
+                     if mtag.type == "neo.event"]
+        self.check_equal_attr(event, nixevents[0])
+        nixspiketrains = [mtag for mtag in nixblk.groups[0].multi_tags
+                          if mtag.type == "neo.spiketrain"]
+        self.check_equal_attr(spiketrain, nixspiketrains[0])
+        nixrcgs = [src for src in nixblk.sources
+                   if src.type == "neo.recordingchannelgroup"]
+        self.check_equal_attr(rcg, nixrcgs[0])
 
     def test_waveforms(self):
         blk = Block()
@@ -182,7 +254,7 @@ class NixIOTest(unittest.TestCase):
         def rand_date():
             return datetime(year=np.random.randint(1980, 2020),
                             month=np.random.randint(1, 13),
-                            day=np.random.randint(1, 30))
+                            day=np.random.randint(1, 29))
 
         def populate_dates(obj):
             obj.file_datetime = rand_date()
@@ -549,7 +621,7 @@ class NixIOTest(unittest.TestCase):
             self.assertEqual(neoobj.file_origin,
                              nixobj.metadata["file_origin"])
         if neoobj.annotations:
-            annotations = nixobj.name+".annotations"
+            annotations = nixobj.metadata.name+".annotations"
             nixannotations = nixobj.metadata.sections[annotations]
             for k, v, in neoobj.annotations.items():
                 self.assertEqual(nixannotations[k], v)
