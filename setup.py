209823a1f62ab4a0f4042eaf3be6fce6bcb0ea72
Achilleas Koutsou <achilleas.k@gmail.com>
2016-02-15 15:55:59 +0200

    Setup script (setup.py) and package information

---
+++ setup.py
@@ -0,0 +1,43 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+import re
+
+__author__ = "Achilleas Koutsou"
+
+install_requires = ["numpy>=1.3.0",
+                    "quantities>=0.9.0"]
+
+with open("README.md") as f:
+    description_text = f.read()
+
+with open("LICENSE") as f:
+    license_text = f.read()
+
+with open("info.py") as f:
+    info = f.read()
+
+VERSION         = re.search(r"VERSION\s*=\s*'([^']*)'", info).group(1)
+AUTHOR          = re.search(r"AUTHOR\s*=\s*'([^']*)'", info).group(1)
+CONTACT         = re.search(r"CONTACT\s*=\s*'([^']*)'", info).group(1)
+BRIEF           = re.search(r"BRIEF\s*=\s*'([^']*)'", info).group(1)
+HOMEPAGE        = re.search(r"HOMEPAGE\s*=\s*'([^']*)'", info).group(1)
+
+
+setup(name             = 'neonix',
+      version          = VERSION,
+      author           = AUTHOR,
+      author_email     = CONTACT,
+      url              = HOMEPAGE,
+      description      = BRIEF,
+      long_description = description_text,
+      license          = 'BSD',
+      packages         = ['neonix', 'neonix.io'],
+      tests_require    = ['nose'],
+      test_suite       = 'nose.collector',
+      requires         = ['nix', 'neo'],
+      package_data     = {'neonix': [license_text, description_text]},
+      include_package_data = True,
+      zip_safe         = False,
+      )
