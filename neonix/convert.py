484d63c7e5487c83473aff4d269582ba36e3e7ab
Achilleas Koutsou <achilleas.k@gmail.com>
2016-02-17 21:46:45 +0200

    Check if read data is iterable
    Close file after reading
    Output formatting

--- neonix/convert.py
+++ neonix/convert.py
@@ -5,21 +5,35 @@ from neonix.io.nixio import NixIO
 
 def main():
     for datafilename in [f for f in os.listdir(".") if os.path.isfile(f)]:
+        # print("\nProcessing {}".format(datafilename))
         try:
             reader = neo.io.get_io(datafilename)
         except OSError:
-            print("File {} does not have an extension known to Neo.".format(
+            print("X {}\n\tdoes not have an extension known to Neo.".format(
                 datafilename
             ))
             continue
-        if neo.core.block.Block in reader.readable_objects:
-            blocks = reader.read_all_blocks()
-        else:
-            print("{} does not support reading blocks.")
+        try:
+            data = reader.read()
+            reader.close()
+        except Exception:
+            # print("\tSomething went wrong while reading data.")
             continue
-        nixfilename = os.path.splitext(datafilename)[0]+"_nix.h5"
-        nixfile = NixIO(nixfilename)
-        nixfile.write_all_blocks(blocks)
+        blocks = []
+        try:
+            blkiter = iter(data)
+        except TypeError:
+            blkiter = iter([data])
+        for item in blkiter:
+            if isinstance(item, neo.core.Block):
+                # filter out non-blocks
+                blocks.append(item)
+        if blocks:
+            nixfilename = os.path.splitext(datafilename)[0]+"_nix.h5"
+            nixfile = NixIO(nixfilename)
+            nixfile.write_all_blocks(blocks)
+            print("* {}\n\tconverted and saved to {}".format(datafilename,
+                                                             nixfilename))
 
 
 if __name__ == "__main__":
